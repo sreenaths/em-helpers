6d94897cdd2b38cc60367d20aa5521939f43d4c2
Sreenath Somarajapuram <sree@apache.org>
2015-12-11 19:45:33 +0530

    Adding txt helper files

---
+++ app/helpers/txt.js
@@ -0,0 +1,1 @@
+export { default, txt } from 'em-helpers/helpers/txt';
---
+++ tests/unit/helpers/txt-test.js
@@ -0,0 +1,10 @@
+import { txt } from '../../../helpers/txt';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | txt');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = txt(42);
+  assert.ok(result);
+});
