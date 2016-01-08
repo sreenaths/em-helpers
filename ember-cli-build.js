a23334552260de0d5b2fa4c25e07adfa19776c2b
Sreenath Somarajapuram <sree@apache.org>
2016-01-08 23:18:15 +0530

    Adding style imports

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,5 +14,9 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
+  app.import("bower_components/snippet-ss/less/no.less");
+  app.import("bower_components/snippet-ss/less/effects.less");
+  app.import("bower_components/bootstrap/less/variables.less");
+
   return app.toTree();
 };
